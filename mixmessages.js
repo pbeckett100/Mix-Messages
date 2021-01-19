27d1d4da32d619ad022a7fa33b2eed429f3b1f63
Patrick Beckett <pbeckett1289@gmail.com>
2021-01-19 13:16:46 +0000

    code added to JS for predict message function

--- mixmessages.js
+++ mixmessages.js
@@ -1,1 +1,16 @@
-//new project//
+
+
+const predictor = () => {
+    const string1 = ['Liverpool ', 'Manchseter United ', 'Arsenal '];
+    const string2 = ['Will definitely ','Might ', 'Will not '];
+    const string3 = ['be CHAMPIONS','finish in the top 4', 'be relegated and cease to exist!']
+
+    const part1 = string1[Math.floor(Math.random() * string1.length)]
+    const part2 = string2[Math.floor(Math.random() * string2.length)]
+    const part3 = string3[Math.floor(Math.random() * string3.length)]
+
+    const fullMessage = part1 + part2 + part3
+    return fullMessage
+}
+
+predictor()
